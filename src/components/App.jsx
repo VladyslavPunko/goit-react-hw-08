73ab6febfba06d740d02c59278afef94a189015b
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-04-28 19:21:23 +0200

    /

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,13 @@ import { useEffect, lazy } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchContacts } from "../redux/contacts/operations";
 import { selectLoading, selectError } from "../redux/contacts/slice";
-import { Route, Routes } from "react-router-dom";
+import { NavLink, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
+import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import ContactsPage from "./pages/ContactsPage/ContactsPage";
+import clsx from "clsx";
+import NotFound from "./NotFound/NotFound";
 
 function App() {
   // const dispatch = useDispatch();
@@ -17,8 +22,20 @@ function App() {
 
   return (
     <>
+      <header>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/login">Log In</NavLink>
+          <NavLink to="/register">Register</NavLink>
+          <NavLink to="/contacts">Contacts</NavLink>
+        </nav>
+      </header>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
+        <Route path="/register" element={<RegistrationPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/contacts" element={<ContactsPage />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </>
   );
---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,7 @@
+import css from "./NotFound.module.css";
+
+const NotFound = () => {
+  return <div className={css.no_page}>Wooops, page is not found!</div>;
+};
+
+export default NotFound;
