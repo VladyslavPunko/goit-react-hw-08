c389a41baa26f99a66cb5516b6a4d97188827561
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-04-28 23:59:18 +0200

    ю

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,13 @@
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-import { selectIsLoggedIn } from "../../redux/auth/slectors";
+import { selectIsLoggedIn, selectUser } from "../../redux/auth/slectors";
+import { useDispatch } from "react-redux";
 
 const Layout = ({ children }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const dispatch = useDispatch();
+  const userData = useSelector(selectUser);
+  const onLogOuot = () => {};
 
   return (
     <div>
@@ -12,8 +16,13 @@ const Layout = ({ children }) => {
           <NavLink to="/">Home</NavLink>
           {isLoggedIn ? (
             <>
-              {" "}
-              <NavLink to="/contacts">Contacts</NavLink>{" "}
+              <NavLink to="/contacts">Contacts</NavLink>
+              <div>
+                <span>Hi, {userData.name}</span>
+                <button type="button" onClick={onLogOuot}>
+                  Logout
+                </button>
+              </div>
             </>
           ) : (
             <>
--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 const HomePage = () => {
-  return <div>Hello amigo</div>;
+  return <div></div>;
 };
 
 export default HomePage;
