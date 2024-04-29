8aa3b8edc5dfdfc3ac98572ac5a1de7f93ab5c72
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-04-30 00:15:10 +0200

    start to css

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,11 +1,27 @@
+import { useDispatch, useSelector } from "react-redux";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import ContactList from "../../components/ContactList/ContactList";
+import { useEffect } from "react";
+import { fetchContacts } from "../../redux/contacts/operations";
+import { selectLoading, selectError } from "../../redux/contacts/slice";
+import Loader from "../../components/Loader/Loader";
+import { ErrorMessage } from "formik";
 
 const ContactsPage = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectLoading);
+  const isError = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <>
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
       <ContactForm />
-      <ContactList />;
+      <ContactList />
     </>
   );
 };
